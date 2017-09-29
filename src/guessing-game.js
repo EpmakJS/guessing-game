b9d9459f358e5697b55138c1c469fefa541baae5
Viktor Ermakov <ermakov_viktor98@mail.ru>
2017-09-30 01:49:52 +0300

    Implementation of Guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        for (min = 0; min <= max; min++){
+            this.arr[min] = min;
+        }
+        this.max = this.arr.length - 1;
     }
 
     guess() {
-
+        this.mid = Math.floor ((this.min + 1 + this.max) / 2.0);
+        return this.arr[this.mid];
     }
 
     lower() {
-
+        return this.max = this.mid;
     }
 
     greater() {
-
+        return this.min = this.mid;
     }
 }
 
-module.exports = GuessingGame;
+
+
+    module.exports = GuessingGame;
